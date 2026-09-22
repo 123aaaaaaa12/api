904d7d98201d90830603a582d1a8fa4f3b40e5b9
netivlyproject <cosielampisz7@gmail.com>
2026-09-22 16:32:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-e
+export default {
+  async fetch(request, env) {
+    try {
+      const result = await env.DB
+        .prepare("SELECT 1 AS test")
+        .first();
+
+      return Response.json({
+        status: "ok",
+        database: result.test === 1
+      });
+    } catch (error) {
+      return Response.json({
+        status: "error",
+        error: error.message
+      }, { status: 500 });
+    }
+  }
+};
