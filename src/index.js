402a66f0cfd1aad5be36d842182826ec331b2888
netivlyproject <cosielampisz7@gmail.com>
2026-09-22 16:38:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -152,22 +152,4 @@ export default {
       status: "online"
     });
   }
-};export default {
-  async fetch(request, env) {
-    try {
-      const result = await env.DB
-        .prepare("SELECT 1 AS test")
-        .first();
-
-      return Response.json({
-        status: "ok",
-        database: result.test === 1
-      });
-    } catch (error) {
-      return Response.json({
-        status: "error",
-        error: error.message
-      }, { status: 500 });
-    }
-  }
 };
